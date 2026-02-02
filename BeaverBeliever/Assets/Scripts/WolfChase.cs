bf16800f6967105ec284c68851c1587b317d8f0b
Shirin-Jain <shirinj@usc.edu>
2026-02-02 13:09:26 -0800

    water prefab for wolf testing

--- BeaverBeliever/Assets/Scripts/WolfChase.cs
+++ BeaverBeliever/Assets/Scripts/WolfChase.cs
@@ -5,8 +5,8 @@ using UnityEngine.Tilemaps;
 public class WolfChase : MonoBehaviour
 {
     public float moveSpeed = 3f;
-    public float jumpDistance = 2f;
-    public float jumpCooldown = 2f;
+    public float jumpSpeed = 6f;
+
     public Tilemap groundTilemap;
     public Tilemap waterTilemap;
 
@@ -14,14 +14,14 @@ public class WolfChase : MonoBehaviour
 
     private Transform player;
     private Rigidbody2D rb;
-    private float lastJumpTime;
 
 
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Beaver").transform;
         rb = GetComponent<Rigidbody2D>();
-        lastJumpTime = Time.time;
+        rb.gravityScale = 0f;
+
 
     }
 
@@ -29,8 +29,7 @@ public class WolfChase : MonoBehaviour
     {
         if (!player) return;
 
-        //Vector2 direction = (player.position - transform.position).normalized;
-        //rb.linearVelocity = direction * moveSpeed;
+    
 
         Vector2 direction = (player.position - transform.position).normalized;
         Vector2 targetPos = rb.position + direction * moveSpeed * Time.fixedDeltaTime;
@@ -48,15 +47,7 @@ public class WolfChase : MonoBehaviour
             rb.linearVelocity = Vector2.zero;
         }
 
-        // Only move if tile exists on Ground layer
-        //if (groundTilemap.HasTile(cell))
-        //{
-        //    rb.MovePosition(targetPos);
-        //}
-        //else
-        //{
-        //    rb.linearVelocity = Vector2.zero;
-        //}
+   
 
 
 
@@ -64,7 +55,6 @@ public class WolfChase : MonoBehaviour
 
     void Update()
     {
-        //TryJump();
 
     }
 
@@ -82,24 +72,11 @@ public class WolfChase : MonoBehaviour
         float distanceToPlayer = Vector2.Distance(transform.position, player.position);
 
 
-        //if (distanceToPlayer > 1.5f && distanceToPlayer < jumpDistance)
-        //{
-        //    Vector2 jumpDir = (player.position - transform.position).normalized;
-        //    rb.AddForce(jumpDir * 5f, ForceMode2D.Impulse);
-        //    lastJumpTime = Time.time;
-        //    JumpVisual();
-        //    Debug.Log("Jump");
-        //}
+     
     }
 
 
-    //IEnumerator JumpVisual()
-    //{
-    //    //Vector3 originalScale = transform.localScale;
-    //    //transform.localScale = originalScale * 5f;
-    //    //yield return new WaitForSeconds(0.15f);
-    //    //transform.localScale = originalScale;
-    //}
+   
 
 
 
