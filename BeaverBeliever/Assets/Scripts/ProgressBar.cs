2a8802497dc5ba86521f16b4bed7174d926c9dd8
Shirin-Jain <shirinj@usc.edu>
2026-02-02 21:45:39 -0800

    dam progres bar

---
+++ BeaverBeliever/Assets/Scripts/ProgressBar.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+public class ProgressBar : MonoBehaviour
+{
+
+
+
+    private DamCollecting damCollector;
+
+    float maxScale;
+    float yScale;
+    public float currentScale = 0;
+
+    Vector3 startLoc;
+
+
+    void Awake()
+    {
+        damCollector = GetComponentInParent<DamCollecting>();
+        maxScale = transform.localScale.x;
+        yScale = transform.localScale.y;
+        startLoc =transform.localPosition;
+
+    }
+    void Update()
+    {
+        float percent = (float)damCollector.collectedCount / damCollector.requiredWood;
+        currentScale = percent * maxScale;
+        transform.localScale = new Vector3(currentScale, yScale, 0f);
+
+        float offsetX = (1f - percent) * maxScale * 0.5f;
+        transform.localPosition = startLoc + Vector3.left * offsetX;
+
+    }
+}
+
